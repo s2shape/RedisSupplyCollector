f7e245cd25beeba61d7a8e6cbe6498c68e2e5c92
Mikhail Burilov <burilovmv@gmail.com>
2019-09-06 13:20:05 +0300

    Added gitlab-ci script, use environment variables for test

--- run-tests.sh
+++ run-tests.sh
@@ -2,18 +2,8 @@
 docker run --name redis -p 6379:6379 -d redis
 docker exec -i redis redis-cli < RedisSupplyCollectorTests/tests/emails-utf8.redis
 
-mkdir RedisSupplyCollectorTests/Properties
-echo { > RedisSupplyCollectorTests/Properties/launchSettings.json
-echo   \"profiles\": { >> RedisSupplyCollectorTests/Properties/launchSettings.json
-echo     \"RedisSupplyCollectorTests\": { >> RedisSupplyCollectorTests/Properties/launchSettings.json
-echo       \"commandName\": \"Project\", >> RedisSupplyCollectorTests/Properties/launchSettings.json
-echo       \"environmentVariables\": { >> RedisSupplyCollectorTests/Properties/launchSettings.json
-echo         \"REDIS_HOST\": \"localhost\", >> RedisSupplyCollectorTests/Properties/launchSettings.json
-echo         \"REDIS_PORT\": \"6379\" >> RedisSupplyCollectorTests/Properties/launchSettings.json
-echo       } >> RedisSupplyCollectorTests/Properties/launchSettings.json
-echo     } >> RedisSupplyCollectorTests/Properties/launchSettings.json
-echo   } >> RedisSupplyCollectorTests/Properties/launchSettings.json
-echo } >> RedisSupplyCollectorTests/Properties/launchSettings.json
+export REDIS_HOST=localhost
+export REDIS_PORT=6379
 
 dotnet build
 dotnet test
